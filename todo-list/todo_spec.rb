8879bd813d698ac8c7af7e1e8793bf2ac6bb1c8e
Josh Nixon & Megan Gaebler <pair+jackmarx.missandry@devbootcamp.com>
2014-10-28 10:21:53 -0500

    defined methods

--- todo-list/todo_spec.rb
+++ todo-list/todo_spec.rb
@@ -1,21 +1,24 @@
+require_relative 'todo'
+require 'rspec'
+
 describe 'todo' do
   describe 'task' do
-    let(:task){ Task.new }
+    let(:task){ Task.new(title: "Buff shoes", description: "With tongue") }
 
     it 'has a title' do
-      expect(task.method_defined?(:title)).to be true
+      expect(task.title).to eq("Buff shoes")
     end
 
     it 'has a description' do
-      expect(task.method_defined?(:description)).to be true
+      expect(task.description).to eq("With tongue")
     end
 
-    it 'has a status' do
-      expect(task.method_defined?(:status)).to be true
+    it 'has a status, and the status is "incomplete" if it is not defined' do
+      expect(task.status).to eq("incomplete")
     end
 
     it 'has a created_at timestamp' do
-      expect(task.method_defined?(:created_at)).to be true
+      expect(task.created_at).to_not be nil
     end
   end
 end
